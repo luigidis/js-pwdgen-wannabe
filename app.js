1bb5fb8715da9fc1b773778400a80d33a9e9a642
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-19 15:47:11 +0200

    Esercizio completo

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// 
+// JAVA DEBUG //
+
+console.log ("ciao")
+
+// CHIEDO IL NOME //
+const nome = prompt('Scrivi il tuo nome')
+
+console.log (nome)
+
+// CHIEDO IL COGNOME //
+const cognome = prompt('Scrivi il tuo cognome')
+
+console.log (cognome)
+
+// CHIEDERE IL COLORE PREFERITO
+const preferedColor = prompt('Scrivi il tuo colore preferito')
+
+console.log (preferedColor)
+
+// GENERO NUMERO RANDOMICO 1 - 100 //
+
+
+// PRENDO L'ELEMENTO HTML DOVE DOVRO' RESTITUIRE LA PASS //
+const randomPass = document.getElementById('random_pass')
+
+console.dir(randomPass)
+
+// RESTITUISCO LA PASSWORD RANDOMICA GENERATA //
+
+randomPass.innerHTML = (nome + cognome + preferedColor + 21)
+
+console.log(nome + cognome + preferedColor + 21)
